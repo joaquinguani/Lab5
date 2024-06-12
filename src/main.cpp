62d61fbc47008845014792d201c294b33e9ac9d7
joaquinguani <joaquin.guani@gmail.com>
2024-06-12 17:23:39 -0300

    cambio algunas ñ por n y tambien el while del main de alta usuario

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #include "DataUsuario.h"
 #include "DataCliente.h"
 #include "DataVendedor.h"
-//comentario de prueba
+//comentario de prueba jejdhdhdhd
 
 int main()
 {
@@ -19,7 +19,7 @@ std::map<std::string,Usuario *> usuarios;
 
 ContUsuario contUsu = ContUsuario(usuarios);
 std::string nick;
-std::string Contraseña;
+std::string Contrasena;
 bool PriemeraVez;
 TFecha* fecha;
 
@@ -29,8 +29,8 @@ while(e) {
 
     printf("      **MERCADO FING**\n");
     printf("            //MENU//\n\n");
-    int TamañoColUsuarios = contUsu.sizeCol();
-    printf("Cantidad usuarios: %d -----> C: %d,  V: %d (falta contar vendedor y cliete por separado)\n\n", TamañoColUsuarios, TamañoColUsuarios, TamañoColUsuarios);
+    int TamanoColUsuarios = contUsu.sizeCol();
+    printf("Cantidad usuarios: %d -----> C: %d,  V: %d (falta contar vendedor y cliete por separado)\n\n", TamanoColUsuarios, TamanoColUsuarios, TamanoColUsuarios);
     printf("precione:\n");
     printf("a: para dar de alta a un usuario\n");
     printf("b: Listado de usuarios\n");
@@ -59,27 +59,26 @@ while(e) {
             char venOcli;
             scanf(" %c", &venOcli); 
             nick = "holaholhola";
-            PriemeraVez = true;
-            while ((usuarios.find(nick) != usuarios.end())|| (PriemeraVez)){
-                if(PriemeraVez){printf("\nIngresar Nickname usuario");}
-                else{printf("\nEse Nickname ya existe, ingrese otro");}
-                std::cin.ignore();
-                std::getline(std::cin, nick);
-                PriemeraVez = false;
-            }
-            printf("\nIngresar Contraseña usuario\n");
+            printf("\nIngresar Nickname usuario");
             std::cin.ignore();
-            std::getline(std::cin, Contraseña);
-            printf("\nIngresar año de nacimiento de usuario\n");
-            int año;
-            scanf("%d", &año);
+            std::getline(std::cin, nick);
+            if (usuarios.find(nick) != usuarios.end()){
+                printf("\nError! Ese nombre de usuario ya existe");
+                break;
+            }
+            printf("\nIngresar Contrasena usuario\n");
+            std::cin.ignore();
+            std::getline(std::cin, Contrasena);
+            printf("\nIngresar ano de nacimiento de usuario\n");
+            int ano;
+            scanf("%d", &ano);
             printf("\nIngresar mes de nacimiento de usuario\n");
             int mes;
             scanf("%d", &mes);
             printf("\nIngresar dia de nacimiento de usuario\n");
             int dia;
             scanf("%d", &dia);
-            fecha = new TFecha(dia, mes, año);
+            fecha = new TFecha(dia, mes, ano);
             if(venOcli == 'a'){
                 printf("\nIngresar ciudad de cliente\n");
                 std::string ciudad;
@@ -93,7 +92,7 @@ while(e) {
                 int numero;
                 scanf("%d",&numero);
                 TDireccion* direccion = new TDireccion(calle, numero);
-                DataCliente* data = new DataCliente(nick,Contraseña,*fecha,*direccion,ciudad);
+                DataCliente* data = new DataCliente(nick,Contrasena,*fecha,*direccion,ciudad);
                 contUsu.ingresarDatosCliente(*data);
             }
             else{
@@ -101,7 +100,7 @@ while(e) {
                 std::string RUT;
                 std::cin.ignore();
                 std::getline(std::cin, RUT);
-                DataVendedor* data = new DataVendedor(nick,Contraseña,*fecha,RUT);
+                DataVendedor* data = new DataVendedor(nick,Contrasena,*fecha,RUT);
                 contUsu.ingresarDatosVendedor(*data);
             }
             
